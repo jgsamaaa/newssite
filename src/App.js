7be8e58f7f5685d2d6f44dbab72334762339bb45
jgsamaaa <jamesgabrieldumaug12@gmail.com>
2022-10-20 11:51:30 -0600

    Done project //need to add some styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from './components/Menu';
 import NewsGrid from './components/NewsGrid';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [active, setActive] = useState(1);
+  const [category, setCategory] = useState('general');
+
+  useEffect(() => {
+    fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=b6f602de5a7f4bb1b1073bf453c7f164`
+    )
+      .then((res) => res.json())
+      .then((data) => setItems(data.articles));
+  }, [category]);
+
   return (
     <div>
       <h1 className="title">See The Latest News</h1>
-      <Menu />
-      <NewsGrid />
+      <Menu active={active} setActive={setActive} setCategory={setCategory} />
+      <NewsGrid items={items} />
     </div>
   );
 }
